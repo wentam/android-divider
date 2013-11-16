10a6ac09e291ae23d45f5b4c8d2e076b0de90def
wentam <wentam42@gmail.com>
2013-11-15 22:31:03 -0500

    1.1.2

--- src/com/wentam/divider/settings.java
+++ src/com/wentam/divider/settings.java
@@ -30,6 +30,7 @@ import android.util.Log;
 
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -43,8 +44,9 @@ import android.widget.AdapterView.OnItemClickListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-
 import android.widget.ArrayAdapter;
+
+import android.net.Uri;
 
 public class settings extends Activity {
     private int widget_id;
@@ -59,13 +61,18 @@ public class settings extends Activity {
 	Bundle extras = intent.getExtras();
 	if (extras != null) {
 	    widget_id = extras.getInt(
-					 AppWidgetManager.EXTRA_APPWIDGET_ID, 
+					 AppWidgetManager.EXTRA_APPWIDGET_ID,
 					 AppWidgetManager.INVALID_APPWIDGET_ID);
 	}
 
 	setContentView(R.layout.orientation);
 
 	ListView l = (ListView) findViewById(R.id.list);
+    TextView ctext = (TextView) findViewById(R.id.ctext);
+    TextView button = (TextView) findViewById(R.id.button);
+
+    ctext.setTextSize(10 * getResources().getDisplayMetrics().density);
+    button.setTextSize(10 * getResources().getDisplayMetrics().density);
 
 	final ArrayAdapter listAdapter = new ArrayAdapter<String>(this, R.layout.list_item, new ArrayList<String>(Arrays.asList(orientations)) );
 	l.setAdapter(listAdapter);
@@ -75,6 +82,12 @@ public class settings extends Activity {
 		    create_widget((int) id);
 		}
 	    });
+
+	// intent test
+	// Intent i = new Intent("com.wentam.defcol.ACTION_SELECT_COLOR");
+	// i.putExtra("startingColor",""+0xFF000000);
+	// i.putExtra("color_id","-1");
+	// startActivityForResult(i,0);
     }
 
     private void create_widget(int orientation) {
@@ -105,5 +118,11 @@ public class settings extends Activity {
 
 
 	finish();
+    }
+    
+
+    public void try_pro(View v) {
+        final Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.wentam.dividerpro"));
+        startActivity(marketIntent);
     }
 }
